5e782cff9f6ba3118b8e6518af62db6423c37b83
mengyao <2533180457@qq.com>
2022-03-10 10:03:38 +0800

    新增书签模块

---
+++ mxy_business/mxy_system/src/main/java/com/mxy/system/service/impl/SysBookmarksServiceImpl.java
@@ -0,0 +1,79 @@
+package com.mxy.system.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mxy.common.core.constant.BaseMessage;
+import com.mxy.common.core.entity.SelfUserEntity;
+import com.mxy.common.core.utils.ServiceResult;
+import com.mxy.system.entity.SysBookmarks;
+import com.mxy.system.entity.vo.SysBookmarksVO;
+import com.mxy.system.mapper.SysBookmarksMapper;
+import com.mxy.system.security.common.util.SecurityUtil;
+import com.mxy.system.service.SysBookmarksService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 收藏书签 服务实现类
+ * </p>
+ *
+ * @author 孟小耀
+ * @since 2022-03-07
+ */
+@Service
+public class SysBookmarksServiceImpl extends ServiceImpl<SysBookmarksMapper, SysBookmarks> implements SysBookmarksService {
+
+    @Override
+    public String getList(SysBookmarksVO sysBookmarksVO) {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        Page<SysBookmarks> page = new Page<>();
+        page.setCurrent(sysBookmarksVO.getCurrentPage());
+        page.setSize(sysBookmarksVO.getPageSize());
+        IPage<SysBookmarks> pageList = this.page(page, queryWrapper);
+        return ServiceResult.success(pageList);
+    }
+
+    @Override
+    public String add(SysBookmarksVO sysBookmarksVO) {
+        SelfUserEntity userDetails = SecurityUtil.getUserInfo();
+        SysBookmarks sysBookmarks = new SysBookmarks();
+        BeanUtils.copyProperties(sysBookmarksVO, sysBookmarks);
+        sysBookmarks.setCreateUser(userDetails.getUsername());
+        Boolean result = sysBookmarks.insert();
+        if (result) {
+            return ServiceResult.successMsg(BaseMessage.INSERT_SUCCESS);
+        } else {
+            return ServiceResult.successMsg(BaseMessage.INSERT_FAIL);
+        }
+    }
+
+    @Override
+    public String edit(SysBookmarksVO sysBookmarksVO) {
+        SelfUserEntity userDetails = SecurityUtil.getUserInfo();
+        SysBookmarks sysBookmarks = new SysBookmarks();
+        BeanUtils.copyProperties(sysBookmarksVO, sysBookmarks);
+        sysBookmarks.setUpdateUser(userDetails.getUsername());
+        Boolean result = sysBookmarks.updateById();
+        if (result) {
+            return ServiceResult.successMsg(BaseMessage.UPDATE_SUCCESS);
+        } else {
+            return ServiceResult.successMsg(BaseMessage.UPDATE_FAIL);
+        }
+    }
+
+    @Override
+    public String delete(SysBookmarksVO sysBookmarksVO) {
+        SysBookmarks sysBookmarks = new SysBookmarks();
+        BeanUtils.copyProperties(sysBookmarksVO, sysBookmarks);
+        Boolean result = sysBookmarks.deleteById();
+        if (result) {
+            return ServiceResult.successMsg(BaseMessage.DELETE_SUCCESS);
+        } else {
+            return ServiceResult.successMsg(BaseMessage.DELETE_FAIL);
+        }
+    }
+
+}
--- mxy_ui/mxy_admin_web/src/views/sys/dictType/index.vue
+++ mxy_ui/mxy_admin_web/src/views/sys/dictType/index.vue
@@ -42,9 +42,9 @@
               :cell-style="{padding:'0px'}" v-loading="typeListLoading"
               element-loading-spinner="el-icon-loading">
               <el-table-column type="index" width="50" align="center"/>
-              <el-table-column prop="dictName" label="名称" align="center"></el-table-column>
-              <el-table-column prop="dictType" label="编码" align="center"></el-table-column>
-              <el-table-column prop="remark" label="备注" align="center"></el-table-column>
+              <el-table-column prop="dictName" show-overflow-tooltip label="名称" align="center"></el-table-column>
+              <el-table-column prop="dictType" show-overflow-tooltip label="编码" align="center"></el-table-column>
+              <el-table-column prop="remark" show-overflow-tooltip label="备注" align="center"></el-table-column>
               <el-table-column label="状态" width="90" class-name="status-col">
                 <template slot-scope="{row}">
                   <el-tooltip v-for="item in statusOptions" v-if="row.status===item.key" :content="item.name"
@@ -111,7 +111,7 @@
               height="550"
               :cell-style="{padding:'0px'}" v-loading="dataListLoading" element-loading-spinner="el-icon-loading">
               <el-table-column type="index" width="50" align="center"/>
-              <el-table-column prop="dictName" label="名称" align="center"></el-table-column>
+              <el-table-column prop="dictName" show-overflow-tooltip label="名称" align="center"></el-table-column>
               <el-table-column prop="dictValue" label="值" align="center"></el-table-column>
               <el-table-column prop="dictSort" label="排序" align="center"></el-table-column>
               <el-table-column label="状态" width="90" class-name="status-col">
@@ -129,7 +129,7 @@
                   </el-tooltip>
                 </template>
               </el-table-column>
-              <el-table-column prop="remark" label="备注" align="center"></el-table-column>
+              <el-table-column prop="remark" show-overflow-tooltip label="备注" align="center"></el-table-column>
               <el-table-column label="操作" align="center" width="230" class-name="small-padding">
                 <template slot-scope="{row,$index}">
                   <el-button size="mini" @click="dataUpdateJump(row)" type="text">编辑</el-button>
