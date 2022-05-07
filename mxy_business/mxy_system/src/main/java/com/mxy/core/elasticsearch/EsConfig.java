7be59c8dfd1cf97199b0060d463505e9240e4c27
mengyao <2533180457@qq.com>
2022-05-07 21:02:37 +0800

    系统优化 1、新增elasticsearch分布式搜索

---
+++ mxy_business/mxy_system/src/main/java/com/mxy/core/elasticsearch/EsConfig.java
@@ -0,0 +1,80 @@
+package com.mxy.core.elasticsearch;
+
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.Node;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Es连接客户端
+ * @author 孟耀
+ * @date 2022-05-07 20:57
+ */
+@Configuration
+public class EsConfig {
+
+    private Logger logger = LoggerFactory.getLogger(EsConfig.class);
+
+    @Value("${elasticsearch.address}")
+    private String address;
+
+    @Value("${elasticsearch.connectTimeoutMin}")
+    private String connectTimeoutMin;
+
+    @Value("${elasticsearch.connectTimeoutMax}")
+    private String connectTimeoutMax;
+
+    @Bean
+    public RestHighLevelClient getHighClient() {
+        return new RestHighLevelClient(getClientBuilder());
+    }
+
+    private RestClientBuilder getClientBuilder() {
+
+        String[] addressArray = address.split(";");
+        int len = addressArray.length;
+        HttpHost[] httpHostsArray = new HttpHost[len];
+        for (int i=0;i<len;i++) {
+            String[] iphost = addressArray[i].split(":");
+            httpHostsArray[i] = new HttpHost(iphost[0], Integer.parseInt(iphost[1]), "http");
+        }
+        // 如果有多个从节点可以持续在内部new多个HttpHost，参数1是ip,参数2是HTTP端口，参数3是通信协议
+        RestClientBuilder clientBuilder = RestClient.builder(httpHostsArray);
+
+        // 添加其他配置，返回来的还是RestClientBuilder对象，这些配置都是可选的
+        // clientBuilder.setXX()...
+
+        // 设置请求头，每个请求都会带上这个请求头
+        Header[] defaultHeaders = {new BasicHeader("header", "value")};
+        clientBuilder.setDefaultHeaders(defaultHeaders);
+
+        // 设置监听器，每次节点失败都可以监听到，可以作额外处理
+        clientBuilder.setFailureListener(new RestClient.FailureListener() {
+            @Override
+            public void onFailure(Node node) {
+                super.onFailure(node);
+                logger.error(node.getName() + "==节点失败了");
+            }
+        });
+
+        clientBuilder.setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
+            @Override
+            public RequestConfig.Builder customizeRequestConfig(RequestConfig.Builder requestConfigBuilder) {
+                // 连接5秒超时，套接字连接60s超时
+                return requestConfigBuilder.setConnectTimeout(Integer.parseInt(connectTimeoutMin)).setSocketTimeout(Integer.parseInt(connectTimeoutMax));
+            }
+        });
+
+        // 最后配置好的clientBuilder再build一下即可得到真正的Client
+        return clientBuilder;
+    }
+}
---
+++ mxy_business/mxy_system/src/main/java/com/mxy/core/elasticsearch/IEsService.java
@@ -0,0 +1,46 @@
+package com.mxy.core.elasticsearch;
+
+/**
+ * @author 孟耀
+ * @date 2022-05-07 20:57
+ */
+public interface IEsService {
+
+    /**
+     * 创建索引库
+     * @param index 索引名称
+     */
+    void createIndexRequest(String index);
+
+    /**
+     * 删除索引库
+     * @param index 索引名称
+     */
+    void deleteIndexRequest(String index);
+
+
+    /**
+     * 更新索引文档
+     * @param index 索引名称
+     * @param id 文档id
+     * @param object 更新对象
+     */
+    void updateRequest(String index, String id, Object object);
+
+
+    /**
+     * 新增索引文档
+     * @param index 索引名称
+     * @param id 文档id
+     * @param object 新增对象
+     */
+    void insertRequest(String index, String id, Object object);
+
+
+    /**
+     * 删除索引文档
+     * @param index 索引名称
+     * @param id 文档id
+     */
+    void deleteRequest(String index, String id);
+}
