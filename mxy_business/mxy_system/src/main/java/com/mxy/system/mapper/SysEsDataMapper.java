ea5eb4f0a16b0acb1e2cd3813f0216c285fb59f1
mengyao <2533180457@qq.com>
2022-05-09 21:57:06 +0800

    系统优化 1、新增ES数据存储，方便进行数据查询测试 2、部分问题修复

---
+++ mxy_business/mxy_system/src/main/java/com/mxy/system/mapper/SysEsDataMapper.java
@@ -0,0 +1,17 @@
+package com.mxy.system.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mxy.common.core.entity.SysEsData;
+
+/**
+ * <p>
+ * ES数据存储 Mapper 接口
+ * </p>
+ *
+ * @author 孟小耀
+ * @since 2022-05-09
+ */
+public interface SysEsDataMapper extends BaseMapper<SysEsData> {
+
+}
---
+++ mxy_common/mxy_common_core/src/main/java/com/mxy/common/core/entity/SysEsData.java
@@ -0,0 +1,153 @@
+package com.mxy.common.core.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.mxy.common.core.entity.vo.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * <p>
+ * ES数据存储
+ * </p>
+ *
+ * @author 孟小耀
+ * @since 2022-05-09
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_es_data")
+@ApiModel(value="SysEsData对象", description="ES数据存储")
+public class SysEsData extends BaseEntity<SysEsData> {
+
+    private static final long serialVersionUID=1L;
+
+    /**
+     * ID
+     */
+      @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 工资
+     */
+    private Integer salary;
+
+    /**
+     * 公司
+     */
+    private String company;
+
+    /**
+     * 性别
+     */
+    private Integer sex;
+
+    /**
+     * 地址
+     */
+    private String address;
+
+    /**
+     * 状态（0正常 1停用）
+     */
+    private String status;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 创建者
+     */
+    private String createUser;
+
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    private Date updateTime;
+
+    /**
+     * 更新者
+     */
+    private String updateUser;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private String isDelete;
+
+    /**
+     * 备用字段一
+     */
+    private String fieldOne;
+
+    /**
+     * 备用字段二
+     */
+    private String fieldTwo;
+
+    /**
+     * 备用字段三
+     */
+    private String fieldThree;
+
+    /**
+     * 备用字段四
+     */
+    private String fieldFour;
+
+    /**
+     * 备用字段五
+     */
+    private String fieldFive;
+
+    /**
+     * 备用字段六
+     */
+    private String fieldSix;
+
+    /**
+     * 备用字段七
+     */
+    private String fieldSeven;
+
+    /**
+     * 备用字段八
+     */
+    private String fieldEight;
+
+    /**
+     * 备用字段九
+     */
+    private Integer fieldNine;
+
+    /**
+     * 备用字段十
+     */
+    private Date fieldTen;
+
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+}
