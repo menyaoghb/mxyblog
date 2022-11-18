77ad6804af1428b4766f0545c44011523744caad
mengyao <2533180457@qq.com>
2022-11-18 17:05:49 +0800

    系统优化 1、登录认证优化

--- mxy_business/mxy_system/src/main/java/com/mxy/security/common/util/SecurityUtil.java
+++ mxy_business/mxy_system/src/main/java/com/mxy/security/common/util/SecurityUtil.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * Security工具类
+ *
  * @Author Mxy
  * @CreateTime 2022/01/2 13:16
  */
@@ -13,25 +14,28 @@ public class SecurityUtil {
     /**
      * 私有化构造器
      */
-    private SecurityUtil(){}
+    private SecurityUtil() {
+    }
 
     /**
      * 获取当前用户信息
      */
-    public static SelfUserEntity getUserInfo(){
-        SelfUserEntity userDetails = (SelfUserEntity) SecurityContextHolder.getContext().getAuthentication() .getPrincipal();
-        return userDetails;
+    public static SelfUserEntity getUserInfo() {
+        return (SelfUserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
+
     /**
      * 获取当前用户ID
      */
-    public static String getUserId(){
+    public static String getUserId() {
         return getUserInfo().getUserId();
     }
+
     /**
      * 获取当前用户账号
      */
-    public static String getUserName(){
+    public static String getUserName() {
         return getUserInfo().getUsername();
     }
+
 }
--- mxy_common/mxy_common_core/src/main/java/com/mxy/common/core/utils/JsonUtils.java
+++ mxy_common/mxy_common_core/src/main/java/com/mxy/common/core/utils/JsonUtils.java
@@ -2,6 +2,13 @@ package com.mxy.common.core.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.util.Map;
 
 /**
  * @Description json工具类
@@ -17,6 +24,48 @@ public class JsonUtils {
     public static String objectToJsonString(Object obj) {
         try {
             String json = JSONObject.toJSONString(obj);
+            return json;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 把json转换为map类型的数据
+     *
+     * @param json
+     * @return
+     */
+    public static Map<String, Object> jsonToMap(String json) {
+
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithModifiers(Modifier.FINAL, Modifier.TRANSIENT, Modifier.STATIC)
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .serializeNulls()
+                .create();
+        Map<String, Object> map = null;
+        try {
+            Type type = new TypeToken<Map<String, Object>>() {
+            }.getType();
+
+            map = gson.fromJson(json, type);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    /**
+     * 把对象转换为json数据
+     *
+     * @param obj
+     * @return 2018年5月7日  下午5:27:16
+     */
+    public static String objectToJson(Object obj) {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+        try {
+            String json = gson.toJson(obj);
             return json;
         } catch (Exception e) {
             e.printStackTrace();
